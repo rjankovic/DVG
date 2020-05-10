379babe60225e9f60937668625626ef19c9d5cee
Radovan Jankovic <jankovic.rj@gmail.com>
2020-05-10 10:55:05 +0200

    cleanup

--- DVG/DVG.Models/DvgConfig.cs
+++ DVG/DVG.Models/DvgConfig.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DVG.Models
 {
-    public class DvgConfig : INotifyPropertyChanged
+    public class DvgConfig
     {
         private List<DvgConfigParameter> _parameters = new List<DvgConfigParameter>();
         public List<DvgConfigParameter> Parameters { get => _parameters; set => _parameters = value; }
@@ -22,7 +22,6 @@ namespace DVG.Models
             get => _dbProjectName; set
             {
                 _dbProjectName = value;
-                OnPropertyChanged();
             }
         }
 
@@ -31,13 +30,6 @@ namespace DVG.Models
 
         private string _vsSolutionPath;
         public string VsSolutionPath { get => _vsSolutionPath; set => _vsSolutionPath = value; }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
 
     }
 
